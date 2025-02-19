a68aea58ff561811315c92f82f8002b6ff9fcca3
Davi Soares <Davi.soares2008@icloud.com>
2025-02-19 16:39:00 -0300

    Delete CreationsOfMethodsPOO directory

--- CreationsOfMethodsPOO/Entities/Entities1.java
+++
@@ -1,9 +0,0 @@
-package org.casa.br.CreationsOfMethodsPOO.Entities;
-
-public class Entities1 {
-
-    public boolean checkAccess(int enteredCode) {
-        int correctCode = 1234;
-        return enteredCode == correctCode;
-    }
-}
--- CreationsOfMethodsPOO/Entities/Entities2.java
+++
@@ -1,7 +0,0 @@
-package org.casa.br.CreationsOfMethodsPOO.Entities;
-
-public class Entities2 {
-    public boolean isSumGreaterThan100(int number1, int number2) {
-        return (number1 + number2) > 100;
-    }
-}
